370833a04ccaf255ea7da53b6a99ba56bfe14e4e
Luiz Cristofori <luiz.araujo@elumini.com.br>
2020-11-03 22:10:46 -0300

    Setup default colors pallete Implement BaseActivity pattern Refactor Main Activity xml layout

--- app/src/main/java/com/example/giphysample/MainActivity.kt
+++ app/src/main/java/com/example/giphysample/MainActivity.kt
@@ -1,28 +1,35 @@
 package com.example.giphysample
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager.widget.ViewPager
+import com.example.giphysample.ui.BaseActivity
 import com.example.giphysample.ui.main.SectionsPagerAdapter
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
+import com.example.giphysample.ui.main.TAB_TITLES
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
+    }
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+    override fun layoutResource(): Int? = R.layout.activity_main
+
+    override fun setupView() {
+        setupToolbar()
+        setupViewPager()
+    }
+
+    private fun setupToolbar() {
+        setSupportActionBar(findViewById(R.id.main_activity_toolbar))
+    }
+
+    private fun setupViewPager() {
+        main_activity_view_pager?.let {
+            it.adapter = SectionsPagerAdapter(lifecycle, supportFragmentManager)
+            TabLayoutMediator(main_activity_tab_layout, it) { tab, position ->
+                tab.text = getString(TAB_TITLES[position])
+            }.attach()
         }
     }
 }
--- app/src/main/java/com/example/giphysample/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/example/giphysample/ui/main/SectionsPagerAdapter.kt
@@ -1,35 +1,28 @@
 package com.example.giphysample.ui.main
 
-import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.giphysample.R
 
-private val TAB_TITLES = arrayOf(
-        R.string.tab_text_1,
-        R.string.tab_text_2
-)
+val TAB_TITLES = arrayOf(R.string.tab_text_1, R.string.tab_text_2)
+
+private const val NUM_PAGES = 2
 
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
-class SectionsPagerAdapter(private val context: Context, fm: FragmentManager)
-    : FragmentPagerAdapter(fm) {
+class SectionsPagerAdapter(lifecycle: Lifecycle, fm: FragmentManager)
+    : FragmentStateAdapter(fm, lifecycle) {
 
-    override fun getItem(position: Int): Fragment {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
+    override fun getItemCount(): Int {
+        return NUM_PAGES
+    }
+
+    override fun createFragment(position: Int): Fragment {
         return PlaceholderFragment.newInstance(position + 1)
-    }
-
-    override fun getPageTitle(position: Int): CharSequence? {
-        return context.resources.getString(TAB_TITLES[position])
-    }
-
-    override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
     }
 }
